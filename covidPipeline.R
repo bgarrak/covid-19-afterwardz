b310a6c36f8949f9dfe064d8e7ddd4f5a1242a59
bgarrak <bgarrak@gmail.com>
2020-05-14 13:04:25 -0700

    Working RODBC connection

--- covidPipeline.R
+++ covidPipeline.R
@@ -2,6 +2,7 @@ library("tidyverse")
 library("RODBC")
 library("sqldf")
 library("stringr")
+library("readr")
 
 
 # Need these libraries, install if not found #
@@ -14,14 +15,40 @@ if(!require(sqldf)){install.packages("sqldf")
 if(!require(stringr)){install.packages("stringr")
 	library(stringr)}
 
+setwd <- "Z:/covidData"
 
-setwd <- "Z:/covidData/"
+################################################################################################
+
+
+### NYT Covid github dataset url ###
+
+dataset_counties_url <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
+
+datset_states_url <- "https://github.com/nytimes/covid-19-data/raw/master/us-states.csv"
+
+
+counties_csv <- read_csv(url(dataset_counties_url))
+
+states _csv <- read_csv(url(dataset_states_url))
+
+################################################################################################
 
 
 ### Use environmental variables for authentication ### 
 u <- Sys.getenv("A_SSMS_Login")
 p <- Sys.getenv("A_SSMS_PWD")
 
+con <- odbcConnect("covidSQLPipe", uid = u, pwd = p)
 
-ch <- odbcConnect("wardzwall.database.windows.net", uid = u, pwd = p)
 
+# test  <- sqlQuery(con, "SELECT * FROM [COVID].[counties]")
+
+
+qMaxDate <- 
+
+
+
+
+
+## CLEAN UP AFTER YOURSELF ##
+odbcCloseAll() 
