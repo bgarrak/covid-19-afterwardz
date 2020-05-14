8ef3c3e2edf74f1bdb1620c041b30c7739b77478
bgarrak <bgarrak@gmail.com>
2020-05-14 15:04:28 -0700

    Added a filter

--- covidPipeline.R
+++ covidPipeline.R
@@ -1,9 +1,10 @@
 library("tidyverse")
 library("RODBC")
 library("sqldf")
-library("stringr")
 library("readr")
+# library("stringr")
 
+################################################################################################
 
 # Need these libraries, install if not found #
 if(!require(tidyverse)){install.packages("tidyverse")
@@ -24,12 +25,12 @@ setwd <- "Z:/covidData"
 
 dataset_counties_url <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
 
-datset_states_url <- "https://github.com/nytimes/covid-19-data/raw/master/us-states.csv"
+# datset_states_url <- "https://github.com/nytimes/covid-19-data/raw/master/us-states.csv"
 
 
 counties_csv <- read_csv(url(dataset_counties_url))
 
-states_csv <- read_csv(url(dataset_states_url))
+# states_csv <- read_csv(url(dataset_states_url))
 
 ################################################################################################
 
@@ -40,15 +41,17 @@ p <- Sys.getenv("A_SSMS_PWD")
 
 con <- odbcConnect("covidSQLPipe", uid = u, pwd = p)
 
-
 # test  <- sqlQuery(con, "SELECT * FROM [COVID].[counties]")
 
 
-qMaxDate <- 
+qMaxDate <- sqlQuery(con, "SELECT max(date) as maxDate FROM [COVID].[counties]")
 
-
+newRecords <- filter(counties_csv, counties_csv$date > qMaxDate)
+head(newRecords)
+ 
 
 
 
 ## CLEAN UP AFTER YOURSELF ##
 odbcCloseAll() 
+
